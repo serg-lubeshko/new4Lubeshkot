a054652922899725b4e02a5dbcd8fd4e7052582e
Sergei_L <lubeshkosergei@gmail.com>
2021-12-28 21:35:19 +0300

    This is the 4th version of the project
    *Moved solutions. Delete database and migrations. Creat them again. Everything is working
    
    If you look at the repository,
    https://github.com/serg-lubeshko/Lubeshko_t_4.git.
    https://github.com/serg-lubeshko/newLubeshko_t4.git
    
    Redid the project 4 times. In old project the problem there was that when
    deleting the migration and the database,
    there was an error with the creating of a new database.
    
    first, I move all models to a new folder
    now, I have a different name for the application

---
+++ course/serializers/serializers_solutions.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from course.models import Solution, Homework
+from course.serializers.serializers_lecture import LectureSerializer
+
+
+class SolutionSerializers(serializers.ModelSerializer):
+    task_solved = serializers.BooleanField(read_only=True)
+
+    class Meta:
+        model = Solution
+        fields = ['id', 'task_solved', 'solution_task', 'homework_solution']
+
+    def validate(self, data):
+        user_id = self.context['request'].user.pk
+
+        homework_id = self.context['request'].data['homework_solution']
+        if Solution.objects.filter(user_solution_id=user_id, homework_solution_id=homework_id).count() > 0:
+            raise serializers.ValidationError("Вы добавили уже решение")
+        return data
+
+
+class HomeworkForSolution(serializers.ModelSerializer):
+    lecture_for_homework = LectureSerializer()
+
+    class Meta:
+        model = Homework
+        fields = ['id', 'title', 'homework_task', 'lecture_for_homework']
---
+++ course/views/solution_view.py
@@ -0,0 +1,34 @@
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from course.conf.permission import IsStudentOrReadOnly
+from course.models import Homework
+from course.serializers.serializers_solutions import SolutionSerializers, HomeworkForSolution
+
+
+@method_decorator(name='post', decorator=swagger_auto_schema(
+    operation_description="Студент отправляет ссылку на решения ро Д/з"))
+@method_decorator(name='get', decorator=swagger_auto_schema(
+    operation_description="Студент смотрит СВОИ домашние работы"))
+class SolutionToHomework(generics.GenericAPIView):
+    """ Решения студентов """
+
+    permission_classes = [IsAuthenticated, IsStudentOrReadOnly]
+    serializer_class = SolutionSerializers
+    queryset = Homework.objects.all()
+
+    def get(self, request):
+        query = Homework.objects.filter(lecture_for_homework__course__student=self.request.user)
+        serializer = HomeworkForSolution(query, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=self.request.data,
+                                           context={'request': request})
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user_solution_id=self.request.user.id, task_solved=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
