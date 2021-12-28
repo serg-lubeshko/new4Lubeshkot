b70b72369f80a92f15bd7fd0b039329a03f049a6
Sergei_L <lubeshkosergei@gmail.com>
2021-12-28 21:21:45 +0300

    This is the 4th version of the project
    *Moved homework. Delete database and migrations. Creat them again. Everything is working
    
    If you look at the repository,
    https://github.com/serg-lubeshko/Lubeshko_t_4.git.
    https://github.com/serg-lubeshko/newLubeshko_t4.git
    
    Redid the project 4 times. In old project the problem there was that when
    deleting the migration and the database,
    there was an error with the creating of a new database.
    
    first, I move all models to a new folder
    now, I have a different name for the application

---
+++ course/serializers/serializers_homework.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+
+from course.models import Lecture, Homework
+from course.serializers.serializers_lecture import LectureSerializer
+
+
+class HomeworkSerializer(serializers.ModelSerializer):
+    professor = serializers.PrimaryKeyRelatedField(read_only=True)
+    lecture_for_homework = serializers.ChoiceField(
+        choices=[i.id for i in Lecture.objects.all()],                #Можно лекции вывессти только свои
+        source='lecture_for_homework_id')
+
+    class Meta:
+        model = Homework
+        fields = ['id', 'title', 'homework_task', 'published_at', 'professor', 'lecture_for_homework']
+
+    def validate(self, data):
+        user_id = self.context['request'].user.pk
+        lecture_id = self.context['request'].data['lecture_for_homework']
+        if Homework.objects.filter(professor_id=user_id, lecture_for_homework=lecture_id).count() > 0:
+            raise serializers.ValidationError("Вы добавили уже домашнюю работу")
+        return data
+
+
+class LectureFofHomework(LectureSerializer):
+    homework = HomeworkSerializer(many=True, source='lecture_for_homework')
+
+    class Meta:
+        model = Lecture
+        fields = ['id', 'title', 'homework']
---
+++ course/views/homework_view.py
@@ -0,0 +1,32 @@
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from course.conf.permission import IsLecturerOrReadOnly
+from course.models import Homework, Lecture
+from course.serializers.serializers_homework import HomeworkSerializer, LectureFofHomework
+
+
+@method_decorator(name='post', decorator=swagger_auto_schema(
+    operation_description="Добавление домашней работы к лекции"))
+@method_decorator(name='get', decorator=swagger_auto_schema(
+    operation_description="Список лекций и домашних работ"))
+class HomeworkToLecture(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated, IsLecturerOrReadOnly]
+    serializer_class = HomeworkSerializer
+    queryset = Homework.objects.all()
+
+    def get(self, request):
+        query = Lecture.objects.filter(professor=self.request.user)
+        serializer = LectureFofHomework(query, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=self.request.data,
+                                           context={'request': request})
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(professor_id=self.request.user.id)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
