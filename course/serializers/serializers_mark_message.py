5ccbb4f68448a3e4ed139d5689c82c0a03b23fb1
Sergei_L <lubeshkosergei@gmail.com>
2021-12-28 21:44:21 +0300

    This is the 4th version of the project
    *Moved mark_message. Delete database and migrations. Creat them again. Everything is working
    
    If you look at the repository,
    https://github.com/serg-lubeshko/Lubeshko_t_4.git.
    https://github.com/serg-lubeshko/newLubeshko_t4.git
    
    Redid the project 4 times. In old project the problem there was that when
    deleting the migration and the database,
    there was an error with the creating of a new database.
    
    first, I move all models to a new folder
    now, I have a different name for the application

---
+++ course/serializers/serializers_mark_message.py
@@ -0,0 +1,130 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from course.models import MessageTeacher, Mark, Solution, Homework, MessageStudent
+from course.serializers.serializers_solutions import SolutionSerializers
+
+MyUser = get_user_model()
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MessageTeacher
+        fields = ['id', 'text', 'published_at']
+
+
+class MarkSerializer(serializers.ModelSerializer):
+    solution_id = serializers.ChoiceField(choices=[i.id for i in Solution.objects.all()])
+    mark = serializers.IntegerField(min_value=0, max_value=10)
+
+    class Meta:
+        model = Mark
+        fields = ['mark', 'solution_id', 'text_message_teacher']
+
+    def validate(self, data):
+        solution_id = self.context['request'].data['solution_id']
+        if Mark.objects.filter(solution_id=solution_id).count() > 0:
+            raise serializers.ValidationError("Вы добавили оценку")
+        return data
+
+
+# _______________________________________________________________________________________________
+class SolutionForProfessorCheckSerializer(serializers.ModelSerializer):
+    id_user = serializers.IntegerField(source='id', read_only=True)
+    user_solution = SolutionSerializers(many=True)
+
+    class Meta:
+        model = MyUser
+        fields = ['id_user', 'username', 'user_solution', ]
+
+
+# _______________________________________________________________________________________________
+class MarkDetailSerializers(serializers.ModelSerializer):
+    solution = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Mark
+        fields = ['mark', 'solution', 'text_message_teacher']
+
+
+# ____________________SolutionForCheckProfessorSerializer______________________________________
+class MarkToProfessorSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Mark
+        fields = ['id', 'mark']
+
+
+class UserToSolutionForCheckProfessorSerializer(serializers.ModelSerializer):
+    id_user = serializers.IntegerField(source='id', read_only=True)
+
+    class Meta:
+        model = MyUser
+        fields = ['id_user', 'username']
+
+
+class HSToSolutionForCheckProfessorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Homework
+        fields = ['id', 'title']
+
+
+class SolutionForCheckProfessorSerializer(serializers.ModelSerializer):
+    user_solution = UserToSolutionForCheckProfessorSerializer()
+    homework_solution = HSToSolutionForCheckProfessorSerializer()
+    mark_solution = MarkToProfessorSerializers()
+
+    class Meta:
+        model = Solution
+        fields = ['id', 'solution_task', 'user_solution', 'homework_solution', 'mark_solution']
+
+
+# ____________________StudentLookSolutionSerializers_______________________________________
+class MarkToStudentSerializers(serializers.ModelSerializer):
+    mark_message = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Mark
+        fields = ['mark', 'mark_message']
+
+
+class StudentLookHisSolutionSerializers(serializers.ModelSerializer):
+    mark_solution = MarkToStudentSerializers()
+    message_solution_teachers = MessageSerializer(many=True)
+
+    class Meta:
+        model = Solution
+        fields = ['id', 'solution_task', 'mark_solution', 'message_solution_teachers']
+
+
+# ___________________StudentMessage___________________________________________________________
+
+class StudentMessageSerializers(serializers.ModelSerializer):
+
+    def __init__(self, *args, **kwargs):
+        super(StudentMessageSerializers, self).__init__(*args, **kwargs)
+        request = self.context.get("request")
+        if not request:
+            return
+        user = self.context["request"].user
+        self.fields["message_solution_students_id"] = serializers.ChoiceField(
+            choices=[i.id for i in Solution.objects.filter(user_solution_id=user.pk)])
+
+    class Meta:
+        model = MessageStudent
+        fields = ['id', 'text', 'published_at', 'message_solution_students_id']
+
+
+# _________________________________ListMessageForProfessor_______________________________________
+
+class MessageStudentSerialezers(serializers.ModelSerializer):
+    class Meta:
+        model = MessageStudent
+        fields = ['text', 'published_at']
+
+
+class ListMessageForProfessorSerialezers(serializers.ModelSerializer):
+    message_solution_students = MessageStudentSerialezers(many=True)
+
+    class Meta:
+        model = Solution
+        fields = ['id', 'solution_task', 'mark_solution', 'message_solution_students']
---
+++ course/views/mark_message_view.py
@@ -0,0 +1,114 @@
+from django.contrib.auth import get_user_model
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from course.conf.permission import IsProfessorOrReadOnlyMark, IsProfessorOrReadOnlyMarkDetail, IsStudentReadOnly
+from course.models import Solution, MessageTeacher, Mark
+from course.serializers.serializers_mark_message import MarkSerializer, SolutionForCheckProfessorSerializer, \
+    MarkDetailSerializers, StudentLookHisSolutionSerializers, StudentMessageSerializers, \
+    ListMessageForProfessorSerialezers
+
+MyUser = get_user_model()
+
+
+@method_decorator(name='post', decorator=swagger_auto_schema(
+    operation_description="Профессор ставит оценку, пишет комментарий"))
+@method_decorator(name='get', decorator=swagger_auto_schema(
+    operation_description="Профессор смотрит решения студентов. Разделил json на решения: "
+                          "проверенные и не проверенные им "))
+class ProfessorWatchHomework(generics.GenericAPIView):
+    """ Профессор смотрит solution и ставит оценку, может написать комментарий"""
+
+    permission_classes = [IsAuthenticated, IsProfessorOrReadOnlyMark]
+    serializer_class = MarkSerializer
+    queryset = MyUser.objects.all()
+
+    def get(self, request):
+        query = Solution.objects.filter(task_solved=True,
+                                        homework_solution__professor_id=request.user.pk)
+        checked_query = query.filter(task_cheked=True)
+        unchecked_query = query.filter(task_cheked=False)
+        checked_serializer = SolutionForCheckProfessorSerializer(checked_query, many=True)
+        unchecked_serializer = SolutionForCheckProfessorSerializer(unchecked_query, many=True)
+        return Response(({'unchecked_solution': unchecked_serializer.data},
+                         {'checked_solution': checked_serializer.data},)
+                        )
+
+    def post(self, request):
+        serializer = self.serializer_class(data=self.request.data,
+                                           context={'request': request})
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user_mark_id=request.user.pk)
+            Solution.objects.filter(id=request.data["solution_id"]).update(task_cheked=True)
+            MessageTeacher.objects.create(message_solution_teachers_id=request.data["solution_id"],
+                                          text=request.data["text_message_teacher"])
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@method_decorator(name='put', decorator=swagger_auto_schema(
+    operation_description="Профессор обновляет оценку, пишет комментарий"))
+class ProfessorMarkDetail(generics.RetrieveUpdateAPIView):
+    """ Профессор обновляет оценку и пишет комментарий"""
+
+    permission_classes = [IsAuthenticated, IsProfessorOrReadOnlyMarkDetail]
+    serializer_class = MarkDetailSerializers
+    queryset = Mark.objects.all()
+    lookup_field = 'solution_id'
+
+    def put(self, request, *args, **kwargs):
+        MessageTeacher.objects.create(message_solution_teachers_id=kwargs['solution_id'],
+                                      text=request.data["text_message_teacher"])
+        return self.update(request, *args, **kwargs)
+
+
+@method_decorator(name='get', decorator=swagger_auto_schema(
+    operation_description="Студент смотрит оценку и комментарии профессора"))
+class StudentLookHisSolution(generics.GenericAPIView):
+    """ Студент смотрит оценку и комментарии профессора"""
+
+    permission_classes = [IsAuthenticated, IsStudentReadOnly]
+    serializer_class = StudentLookHisSolutionSerializers
+
+    def get(self, request):
+        query = Solution.objects.filter(task_solved=True,
+                                        user_solution_id=request.user.pk)
+        verified_query = query.filter(task_cheked=True)
+        unverified_query = query.filter(task_cheked=False)
+        verified_serializer = StudentLookHisSolutionSerializers(verified_query, many=True)
+        unverified_serializer = StudentLookHisSolutionSerializers(unverified_query, many=True)
+        return Response(({'verified_homework': verified_serializer.data},
+                         {'unverified_homework': unverified_serializer.data})
+                        )
+
+
+@method_decorator(name='post', decorator=swagger_auto_schema(
+    operation_description="Студент пишет комментарий к Solution"))
+class StudentMessage(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated, IsStudentReadOnly]
+    serializer_class = StudentMessageSerializers
+
+    # def get(self, request, solution_id):
+    #     query = Solution.objects.get(id=solution_id)
+    #     serializer = StudentMessageGETSerializers(query)
+    #     return Response(serializer.data)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=self.request.data,
+                                           context={'request': request})
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user_message_id=request.user.pk)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_description="Проффесор смотрит сообщения студента к его Solution"))
+class ListMessageForProfessor(generics.ListAPIView):
+    permission_classes = [IsAuthenticated, IsProfessorOrReadOnlyMarkDetail]
+    serializer_class = ListMessageForProfessorSerialezers
+
+    def get_queryset(self):
+        return Solution.objects.filter(task_solved=True,
+                                       homework_solution__professor_id=self.request.user.pk)
